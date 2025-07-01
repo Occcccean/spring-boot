733a5acfd4f6e646a4818ae566e48bec91fd3632
Occcccean <161466923+Occcccean@users.noreply.github.com>
2025-07-01 14:46:53 +0800

    first commit

---
+++ src/main/java/lingshin/meteor/web/课设Application.java
@@ -0,0 +1,13 @@
+package lingshin.meteor.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 课设Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(课设Application.class, args);
+	}
+
+}
---
+++ src/test/java/lingshin/meteor/web/课设ApplicationTests.java
@@ -0,0 +1,13 @@
+package lingshin.meteor.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class 课设ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
